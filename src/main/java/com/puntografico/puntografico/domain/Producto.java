f940f2ed62c5899ca2e7f6904439666ad74bc206
benimhoff <imhoff.bennett@gmail.com>
2026-03-02 15:29:38 -0300

    - Mostrar los detalles de cada item en el orden que estén en el producto.

--- src/main/java/com/puntografico/puntografico/domain/Producto.java
+++ src/main/java/com/puntografico/puntografico/domain/Producto.java
@@ -1,9 +1,14 @@
 package com.puntografico.puntografico.domain;
 
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.Getter;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
 
 @Entity
 @Table(name = "producto")
@@ -23,4 +28,17 @@ public class Producto {
 
     @Column(columnDefinition = "TEXT")
     private String iconoSvg;
+
+    @Transient // Esto indica que no es una columna de la base de datos
+    public List<Map<String, Object>> getCamposList() {
+        if (this.esquemaConfiguracion == null || this.esquemaConfiguracion.isEmpty()) {
+            return Collections.emptyList();
+        }
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(this.esquemaConfiguracion, new TypeReference<List<Map<String, Object>>>() {});
+        } catch (Exception e) {
+            return Collections.emptyList();
+        }
+    }
 }
